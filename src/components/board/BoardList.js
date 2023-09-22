6ceef3b6b8f7159327f20e257f5328bfe1f8908b
Lee-yrim <reasonn2323@gmail.com>
2023-09-22 18:19:14 +0900

    230922 update in class

--- src/components/board/BoardList.js
+++ src/components/board/BoardList.js
@@ -7,23 +7,12 @@ import PageNavigation from "./PageNavigation";
 
 const BoardList = () => {
   const { currentPage } = useParams();
-  //  useParams() 함수를 통해 얻은 객체에서 currentPage라는 속성을 추출하는 코드
   const dispatch = useDispatch();
-  // 리액트 프로젝트에서 Redux를 사용할 때 Redux store로부터 dispatch 함수를 추출하는 코드
+
   const getBoardList = (currentPage) => {
     dispatch(boardActions.getBoardList(currentPage));
   };
-
-  /* 
-  Redux의 주요 개념은 다음과 같습니다:
-
-Store: 애플리케이션의 상태를 저장하는 중앙 저장소입니다.
-
-Action: 상태 변경을 요청하는 객체입니다. 액션은 일반적으로 JavaScript 객체이며, 상태 변경의 유형과 함께 데이터를 포함합니다.
-
-Reducer: 액션에 따라 상태를 어떻게 변경할지 정의하는 함수입니다. 이전 상태와 액션을 받아 새로운 상태를 반환합니다.
-
-Dispatch: 액션을 Redux store로 보내는 함수입니다. dispatch 함수를 호출하면 액션을 store에 전달하고, 이에 따라 상태가 변경됩니다. */
+  //board_action에서 정의된 getBoardList라는 함수를 여기서 호출해서 쓸수 있게 해주는 거임
 
   useEffect(() => {
     getBoardList(currentPage);
@@ -59,6 +48,8 @@ Dispatch: 액션을 Redux store로 보내는 함수입니다. dispatch 함수를
           {boardList &&
             boardList.map((board) => {
               return <TableRow board={board} key={board.num} />;
+              //보드리스트 배열을 map함수를 통해서 순회하는 부분
+              //반복해서 보드리스트의 각 요소를 TableRow로 렌더링 해주는거
             })}
         </tbody>
       </table>
